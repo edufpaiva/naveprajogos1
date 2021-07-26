07708ba479a727cc820653eef7dea1d7de942244
Dayane Cristine Leite <daycristine22@gmail.com>
2021-07-25 22:53:27 -0400

    menuController

---
+++ nave/Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	//public Animator cometa1, cometa2;
+	// Use this for initialization
+	void Start(){
+		//cometa1.SetBool("turnLeft", false);
+	}
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+
+        }
+
+    }
+	public void CarregarCena(string nomeDaCena){
+		UnityEngine.SceneManagement.SceneManager.LoadScene (nomeDaCena);
+	}
+}
