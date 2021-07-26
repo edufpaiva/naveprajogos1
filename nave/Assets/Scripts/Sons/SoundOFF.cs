c6e7b3e62b37757e37da2a2f0fbf8a75094370f4
Dayane Cristine Leite <daycristine22@gmail.com>
2021-07-25 20:42:21 -0400

    adding soundoff.cs

---
+++ nave/Assets/Scripts/Sons/SoundOFF.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOFF : MonoBehaviour {
+    private AudioSource sons;
+    public float volume;
+    // Use this for initialization
+    void Awake() {
+        sons = GetComponent<AudioSource>();
+    }
+        
+
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        if (GameManager.Instance.GetMudo())
+        {
+            sons.volume = 0;
+        }
+        else {
+            sons.volume = volume;
+            
+        }
+    }
+}
