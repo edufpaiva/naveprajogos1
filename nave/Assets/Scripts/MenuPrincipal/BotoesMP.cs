5bbee7f44dea8ebde7cd433fb855e71dfbf5bd95
Dayane Cristine Leite <daycristine22@gmail.com>
2021-07-26 23:56:26 -0400

    animation sound
    animation sound
    transition from menu to gameplay

---
+++ nave/Assets/Scripts/MenuPrincipal/BotoesMP.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotoesMP : MonoBehaviour {
+    private bool som, creditoAtivado;
+    public GameObject creditos;
+    private Animator mAnimator;
+
+
+    void Awake()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+    // Use this for initialization
+    void Start () {
+
+        
+        
+        if (GameManager.Instance.GetMudo())
+        {
+            mAnimator.SetBool("SomLigado", false);
+
+        }
+        else {
+            mAnimator.SetBool("SomLigado", true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AtivaSom() {
+        
+        GameManager.Instance.SetMudo();
+
+        mAnimator.SetBool("SomLigado", !GameManager.Instance.GetMudo());
+
+
+
+
+    }
+
+
+
+    public void AtivaCredito() {
+
+        creditos.SetActive(GameManager.Instance.GetCreditoAtivo());
+        GameManager.Instance.SetCreditoAtivo();
+
+    }
+}
